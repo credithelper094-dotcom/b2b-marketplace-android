af48ae38e9466633890ce7b30691c000744ccfe4
credithelper094-dotcom <credithelper094@gmail.com>
2026-09-01 16:19:49 +0300

    build: add Retrofit networking dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,5 +44,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
